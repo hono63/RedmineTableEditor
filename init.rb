5568c55c07fd3f813aa845541a6c302f49f5dd26
hono63 <hono6363@yahoo.co.jp>
2021-02-23 17:43:00 +0900

    u

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 require_dependency 'rte_hook_listener'
 
 Redmine::Plugin.register :redmine_table_editor do
-  name 'Redmine Table Editor plugin'
+  name 'Table Editor plugin'
   author 'hono63'
-  description 'This is a plugin for Redmine'
+  description 'このプラグインは views/issues/_list.html.erb を置き換えます。'
   version '0.0.1'
   url 'https://github.com/hono63/RedmineTableEditor'
   author_url 'https://github.com/hono63'
--- lib/rte_hook_listener.rb
+++ lib/rte_hook_listener.rb
@@ -1,5 +1,9 @@
 class RteHookListener < Redmine::Hook::ViewListener
-    def view_projects_show_left(context = {})
-        return content_tag("p", "Custom content on left side")
+    def view_issues_index_bottom(context = {})
+        # return content_tag("p", "Custom content on left side")
+        return <<-EOS
+<link href="https://unpkg.com/tabulator-tables@4.9.3/dist/css/tabulator.min.css" rel="stylesheet">
+<script type="text/javascript" src="https://unpkg.com/tabulator-tables@4.9.3/dist/js/tabulator.min.js"></script>
+EOS
     end
 end
