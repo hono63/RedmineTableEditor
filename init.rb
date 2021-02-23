40835379a64bb05668718be1f4faa57b5ced9213
hono63 <hono6363@yahoo.co.jp>
2021-02-23 16:05:11 +0900

    u

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_table_editor do
   name 'Redmine Table Editor plugin'
-  author 'Author name'
+  author 'hono63'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/hono63/RedmineTableEditor'
+  author_url 'https://github.com/hono63'
 end
