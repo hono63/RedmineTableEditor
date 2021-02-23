3ee012414df3fc4d6b67b0ae98ce59cea785b1e1
hono63 <hono6363@yahoo.co.jp>
2021-02-23 18:27:07 +0900

    日本語は使えねえ

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require_dependency 'rte_hook_listener'
 Redmine::Plugin.register :redmine_table_editor do
   name 'Table Editor plugin'
   author 'hono63'
-  description 'このプラグインは views/issues/_list.html.erb を置き換えます。'
+  description 'This plugin replaces views/issues/_list.html.erb'
   version '0.0.1'
   url 'https://github.com/hono63/RedmineTableEditor'
   author_url 'https://github.com/hono63'
