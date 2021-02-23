2fbd0fe76862771534228b7e281b56a06e0725ab
hono63 <hono6363@yahoo.co.jp>
2021-02-23 18:23:22 +0900

    u

--- lib/rte_hook_listener.rb
+++ lib/rte_hook_listener.rb
@@ -1,12 +1,12 @@
 class RteHookListener < Redmine::Hook::ViewListener
     def view_issues_index_bottom(context = {})
-        # return content_tag("p", "Custom content on left side")
-        return <<-EOS
-<link href="https://unpkg.com/tabulator-tables@4.9.3/dist/css/tabulator.min.css" rel="stylesheet">
-<script type="text/javascript" src="https://unpkg.com/tabulator-tables@4.9.3/dist/js/tabulator.min.js"></script>
-<script type="text/javascript">
-alert("Hello");
-</script>
-EOS
+        return content_tag("p", "Custom content on bottom")
+        #return <<"EOS"
+#<link href="https://unpkg.com/tabulator-tables@4.9.3/dist/css/tabulator.min.css" rel="stylesheet">
+#<script type="text/javascript" src="https://unpkg.com/tabulator-tables@4.9.3/dist/js/tabulator.min.js"></script>
+#<script type="text/javascript">
+#alert("Hello");
+#</script>
+#EOS
     end
 end
